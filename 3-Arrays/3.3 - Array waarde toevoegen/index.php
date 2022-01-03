184ca6a75bca15d7012c7880b4762decda51d02d
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-01-03 21:11:03 +0100

    3.3

---
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -0,0 +1,7 @@
+<?php
+$myHeroes = ['snelman' , 'thanoos' , 'megachin'];
+echo $myHeroes[0] . "<br>";
+echo $myHeroes[1] . "<br>";
+echo $myHeroes[2] . "<br>";
+array_push($myHeroes, 'GT');
+print_r($myHeroes);
