db18fa7980c6920ae9e6b5b71c99f4715aa638a5
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-01-03 21:00:48 +0100

    3.2

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,11 @@
+<?php
+$myDogNames = [ "anass" , "jack" , "man" ];
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br><br>" ;
+
+$myDogNames[0] = 'jakcjack';
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br>";
+?>
