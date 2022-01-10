7cca1517b128d8409f35423eddb04c6c980cc4d5
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-01-10 15:37:28 +0100

    new

--- 7-Working with files/7.3 - html & php/index.php
+++ 7-Working with files/7.3 - html & php/index.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php  
+    $leeftijd = 17;
+?>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
-    <div id="isMeerderJarig">
+
+    <div id="IsMeerderJarig">
         <?php
-        $leeftijd = 12;
-        if($leeftijd<18){
-            echo "Je hebt geen toegang tot deze website.";
-        }
+            if($leeftijd < 18 ){
+                echo "<h6>Je bent te jong om deze pagina te bekijken</h6>";
+            }
         ?>
     </div>
 
