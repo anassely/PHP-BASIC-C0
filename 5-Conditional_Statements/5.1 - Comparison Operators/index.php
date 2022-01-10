fec236e48aa2d31ec8691d95ee4e7af8529121e9
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-01-10 14:53:23 +0100

    new

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,11 @@
+<?php
+$test1 = 11;
+$test2 = "hoi";
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
