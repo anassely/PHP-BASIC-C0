a5d3bacefce3806f41f7ab25e2dcf393571b823f
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-02-21 19:59:35 +0100

    new

--- 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
 <?php
-$voornaam1 = "anass";
-$voornaam2 = "nassim";
-$voornaam3 = "rayan";
-echo "<li>$voornaam1 </li> <li> $voornaam2</li>  <li>$voornaam3</li>";
+$voornaam_a = "anass";
+$voornaam_b = "rayan";
+$voornaam_c = "Nassin";
+
+echo "<ul> <li>$voornaam_a</li> <li>$voornaam_b</li> <li>$voornaam_c</li> </ul>";
 ?>
-</body>
-</html>
