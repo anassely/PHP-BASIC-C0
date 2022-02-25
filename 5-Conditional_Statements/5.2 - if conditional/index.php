7a03ed98d2427e524e1590bbc5d7144b067bc79a
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-02-25 22:08:15 +0100

    new

--- 5-Conditional_Statements/5.2 - if conditional/index.php
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -1,33 +1,18 @@
 <?php
 $test1 = 10;
-
-$test2 = 10;
-
-if($test1 == $test2){
-    echo("gelijk <br>");
+$test2 = 6;
+if($test1 == $test2); {
+    echo "Ongelijk" . "<br>";
 }
 
-if($test1 != $test2){
-    echo("ongelijk <br>");
+if($test1 === $test2); {
+    echo "Identiek" . "<br>";
 }
 
-if($test1 === $test2){
-    echo("identiek <br>");
+if($test1 > $test2); {
+    echo "test1 is groter dan test2" . "<br>";
 }
 
-if($test1 > $test2){
-    echo("test1 is groter dan test2 <br>");
+if($test1 <> $test2); {
+    echo "Test 1 is groter of gelijk aan test2";
 }
-
-if($test1 < $test2){
-    echo("test1 is kleiner dan test2 <br>");
-}
-
-if($test1 >= $test2){
-    echo("test1 is groter of gelijk aan test2 <br>");
-}
-
-if($test1 <= $test2){
-    echo("test1 is kleiner of gelijk aan test2 <br>");
-}
-?>
