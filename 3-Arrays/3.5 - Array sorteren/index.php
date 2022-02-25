eb8f1e708c826eb200017cbeef67386371011808
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-02-25 22:02:55 +0100

    new

--- 3-Arrays/3.5 - Array sorteren/index.php
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -1,11 +1,6 @@
-
-   
 <?php
-    $coders =  [676,5345,78657,3,46,21,47,];
-    asort ($coders);
-    print_r($coders);
-
-    $laag_hoog = [3,21,46,47,676,5345,78657];
-    arsort ($laag_hoog);
-    print_r($laag_hoog);
-    ?>
+$getallen = [676, 5345, 78657, 3, 46, 21, 47];
+asort($getallen);
+$laag_hoog = $getallen;
+print_r($laag_hoog);
+?>
